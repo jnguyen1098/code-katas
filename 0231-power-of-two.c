2497b62941c52fc0a35e8b93f4fdfdfa479c1fbe
Jason <40174810+jnguyen1098@users.noreply.github.com>
2020-06-08 12:04:35 -0400

    Update 0231-power-of-two.c

--- 0231-power-of-two.c
+++ 0231-power-of-two.c
@@ -1,10 +1,13 @@
+// Explanation:
+
 // 16 = 1000
 // 15 = 0111
 
 // 32 = 10000
 // 31 = 01111
 
-// Notice how the ones are mutually exclusive. If we mask them using &, it should be return 0.
+// Notice how the ones between a power of two and its increment are mutually-exclusive.
+// That means if we mask them using &, it should be return 0.
 
 bool isPowerOfTwo(int n)
 {
