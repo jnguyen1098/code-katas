a9797dacb72cd811443dd2e2ae9d0929b127bcc1
Jason <40174810+jnguyen1098@users.noreply.github.com>
2020-10-07 13:46:51 -0400

    Update 0061-rotate-list.c

--- 0061-rotate-list.c
+++ 0061-rotate-list.c
@@ -1,43 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
 struct ListNode *rotateRight(struct ListNode *head, int k)
 {
-    if (!head) {
-        return NULL;
-    }
-    
-    if (!k) {
+    // No work if NULL list or k == 0
+    if (!head || !k) {
         return head;
     }
     
+    // Find last node and keep track of length
     size_t length = 1;
     struct ListNode *tmp = NULL;
     for (tmp = head; tmp->next; tmp = tmp->next) {
         length++;
     }
     
+    // Create circular linked list
     tmp->next = head;
-    
-    size_t start = length - (k % length);
-    
+
+    // Go to the new start of the list    
     tmp = head;
-    for (int i = 0; i < start; i++) {
+    for (int i = 0; i < length - (k % length); i++) {
         tmp = tmp->next;
     }
     
-    struct ListNode *return_ptr = tmp;
+    // Update to the new head
+    head = tmp;
     
+    // From this point on, find the new tail
     for (int i = 0; i < length - 1; i++) {
         tmp = tmp->next;
     }
     
+    // Update tail
     tmp->next = NULL;
     
-    return return_ptr;
+    // Return updated head
+    return head;
 }
