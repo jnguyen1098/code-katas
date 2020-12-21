ffccabdf4bcfe4366aba8b2318ee0a840ad7f661
Jason <40174810+jnguyen1098@users.noreply.github.com>
2020-12-21 15:20:10 -0500

    Create 0001-two-sum.py

---
+++ 0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        foo = {}
+        for i, val in enumerate(nums):
+            complement = target - val
+            if complement in foo:
+                return [i, foo[complement]]
+            foo[val] = i
