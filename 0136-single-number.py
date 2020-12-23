7422debf67cf6ce4f6ef2ec993b736223de60eff
Jason <40174810+jnguyen1098@users.noreply.github.com>
2020-12-23 17:52:15 -0500

    Create 0136-single-number.py

---
+++ 0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        missing = 0
+        
+        for num in nums:
+            missing ^= num
+        
+        return missing
