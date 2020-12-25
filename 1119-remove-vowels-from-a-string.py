5e971640dd75f1df66df58555401c72f488de742
Jason <40174810+jnguyen1098@users.noreply.github.com>
2020-12-25 11:13:30 -0500

    Update 1119-remove-vowels-from-a-string.py

--- 1119-remove-vowels-from-a-string.py
+++ 1119-remove-vowels-from-a-string.py
@@ -7,3 +7,9 @@ class Solution:
                 result += char
         
         return result
+    
+"""
+class Solution:
+    def removeVowels(self, s: str) -> str:
+        return "".join(c for c in s if c not in "aeiou")
+"""
