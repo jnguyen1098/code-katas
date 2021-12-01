7cf76c67fecd3213dadc159d201679ff34c62caa
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-01 09:13:04 -0500

    part2

--- advent_of_code_2021/day01/part_2.py
+++ advent_of_code_2021/day01/part_2.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
+from collections import deque
+
 fin = open("input", "r").read().split()
-lines = [line for line in fin]
+depths = [int(line) for line in fin]
 count = 0
-for i in range(1, len(lines)):
-    if int(lines[i]) > int(lines[i - 1]):
+
+window = deque([])
+cum_sum = 0
+windows = []
+
+for depth in depths:
+    window.append(depth)
+    cum_sum += depth
+    if len(window) == 3:
+        windows.append(cum_sum)
+        cum_sum -= window.popleft()
+
+for i in range(1, len(windows)):
+    if windows[i] > windows[i - 1]:
         count += 1
+
 print(count)
