4d774e20daac0af348e05eb680fc531c18ca5dae
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-04 00:00:21 -0500

    day04 boiler

---
+++ advent_of_code_2021/day04/part_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+from math import gcd, floor, sqrt, log
+from collections import defaultdict, deque
+from bisect import bisect_left, bisect_right
+
+MOD = 1000000007
+
+delim = "\n"
+
+lines = open("input", "r").read().splitlines()
+
+for idx, line in enumerate(lines):
+    print(idx, line)
+print("linecount:", len(lines))
