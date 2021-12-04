1b7469ac5e5e96ec80cca9e93597027f9a50e48a
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-04 01:44:23 -0500

    part2

--- advent_of_code_2021/day04/part_2.py
+++ advent_of_code_2021/day04/part_2.py
@@ -25,8 +25,26 @@ class Board:
                 self.idx[self.rows[i][j]] = (i, j)
 
     def mark(self, num, mark):
-        location = self.idx[num]
-        self.rows[location[0]][location[1]] = mark
+        location = self.idx.get(num)
+        if location:
+            self.rows[location[0]][location[1]] = mark
+
+    def win(self):
+        for row in self.rows:
+            if set(row) == set([-1]):
+                return True
+        for col in list(zip(*self.rows)):
+            if set(col) == set([-1]):
+                return True
+        return False
+
+    def get_score(self, winning_move):
+        score = 0
+        for row in self.rows:
+            for num in row:
+                if num != -1:
+                    score += num
+        return score * winning_move
 
     def __str__(self):
         return "\n".join(str(row) for row in self.rows)
@@ -46,3 +64,136 @@ for idx, board in enumerate(boards):
     print(idx)
     print(board)
     print()
+    
+assert(
+    Board(
+        [
+            [-1, -1, -1, -1, -1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, 1, 1, 1],
+            [-1, -1, -1, -1, -1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [-1, -1, -1, -1, -1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [-1, -1, -1, -1, -1],
+            [1, 1, 1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [-1, -1, -1, -1, -1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [-1, 1, 1, 1, 1],
+            [-1, 1, 1, 1, 1],
+            [-1, 1, 1, 1, 1],
+            [-1, 1, 1, 1, 1],
+            [-1, 1, 1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, -1, 1, 1, 1],
+            [1, -1, 1, 1, 1],
+            [1, -1, 1, 1, 1],
+            [1, -1, 1, 1, 1],
+            [1, -1, 1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, -1, 1, 1],
+            [1, 1, -1, 1, 1],
+            [1, 1, -1, 1, 1],
+            [1, 1, -1, 1, 1],
+            [1, 1, -1, 1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, 1, -1, 1],
+            [1, 1, 1, -1, 1],
+            [1, 1, 1, -1, 1],
+            [1, 1, 1, -1, 1],
+            [1, 1, 1, -1, 1],
+        ]
+    ).win()
+)
+
+assert(
+    Board(
+        [
+            [1, 1, 1, 1, -1],
+            [1, 1, 1, 1, -1],
+            [1, 1, 1, 1, -1],
+            [1, 1, 1, 1, -1],
+            [1, 1, 1, 1, -1],
+        ]
+    ).win()
+)
+
+wins = set()
+
+for num in numbers:
+    for idx, board in enumerate(boards):
+        if idx in wins: continue
+        board.mark(num, -1)
+        if board.win():
+            wins.add(idx)
+            print(f"{idx} won on {num}")
+            print(board)
+            print(f"score: {board.get_score(num)}")
+            print()
