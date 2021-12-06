c4d306e3513dece19ec487dc3063af0eebba0eb7
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-06 00:04:00 -0500

    fish

---
+++ advent_of_code_2021/day06/part_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+from math import gcd, floor, sqrt, log
+from collections import defaultdict, deque
+from bisect import bisect_left, bisect_right
+
+MOD = 1000000007
+
+lines = open("input", "r").read().splitlines()
+
+fish = [int(tmp) for tmp in lines[0].split(",")]
+
+days = 80
+
+for _ in range(days):
+    for i in range(len(fish)):
+        if fish[i] == 0:
+            fish.append(8)
+            fish[i] = 6
+        else:
+            fish[i] -= 1
+
+print(len(fish))
