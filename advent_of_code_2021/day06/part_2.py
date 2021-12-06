7ee370ed2758f78ebcce13d3dfc98935d5764a46
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-06 01:15:41 -0500

    remove heapq

--- advent_of_code_2021/day06/part_2.py
+++ advent_of_code_2021/day06/part_2.py
@@ -9,42 +9,33 @@ lines = open("input", "r").read().splitlines()
 
 fish = [int(tmp) for tmp in lines[0].split(",")]
 
-from heapq import *
-
-days = 18
+days = 80
 
 reqs = []
 result = []
 
-expected = [6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8]
-
 for i in range(len(fish)):
     adder = 1
     while fish[i] + adder <= days:
-        heappush(reqs, fish[i] + adder)
+        reqs.append(fish[i] + adder)
         adder += 7
     result.append((fish[i] - days) % 7)
 
 while reqs:
-    popped = heappop(reqs)
-    print(popped)
+    popped = reqs.pop()
+    if popped == days:
+        result.append(8)
+        continue
     adder = 1
-    while popped + adder + 8 <= days:
-        heappush(reqs, popped + adder + 8)
+    tmp = popped + 8
+    while tmp + adder <= days:
+        reqs.append(tmp + adder)
         adder += 7
     if popped == days - 2:
         result.append(6)
     elif popped == days - 1:
         result.append(7)
     else:
-        result.append(  (6 - days - popped - 2)  % 7)
+        result.append(  (8 - (days - popped))  % 7)
 
-if len(result) != len(expected):
-    print("len differ")
-
-print(result)
 print(len(result))
-
-assert result == [6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8]
-
-
