1b017e333fe7c4d8be57616d4689cdc2a7831334
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-07 00:00:41 -0500

    day07

---
+++ advent_of_code_2021/day07/part_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+from math import gcd, floor, sqrt, log
+from collections import defaultdict, deque
+from bisect import bisect_left, bisect_right
+
+MOD = 1000000007
+
+tmp_data = [int(tmp) for tmp in open("input", "r").read().split(",")]
+
+print(tmp_data)
