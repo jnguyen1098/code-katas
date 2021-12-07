477a82c34bb58bac36a4b5b44619fca0f9eaa363
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-07 09:34:15 -0500

    update

--- advent_of_code_2021/day07/part_1.py
+++ advent_of_code_2021/day07/part_1.py
@@ -3,14 +3,7 @@
 import sys
 import math
 
-from math import gcd, floor, sqrt, log
-from collections import defaultdict, deque
-from bisect import bisect_left, bisect_right
-
-MOD = 1000000007
-
 tmp_data = [int(tmp) for tmp in open("input", "r").read().split(",")]
-#tmp_data = [int(tmp) for tmp in open("example", "r").read().split(",")]
 
 def move_all_to(crabs, num):
     cost = 0
--- advent_of_code_2021/day07/part_2.py
+++ advent_of_code_2021/day07/part_2.py
@@ -3,19 +3,23 @@
 import sys
 import math
 
-from math import gcd, floor, sqrt, log
-from collections import defaultdict, deque
-from bisect import bisect_left, bisect_right
-
-MOD = 1000000007
-
 tmp_data = [int(tmp) for tmp in open("input", "r").read().split(",")]
-#tmp_data = [int(tmp) for tmp in open("example", "r").read().split(",")]
+
+def sum_of_n(n):
+    s = 0
+    for i in range(1, n + 1):
+        s += i
+    return s
+
+def get_triangle(n):
+    return (n * (n + 1)) // 2
+
+strategy = get_triangle
 
 def move_all_to(crabs, num):
     cost = 0
     for i in range(len(crabs)):
-        cost += abs(crabs[i] - num)
+        cost += strategy(abs(crabs[i] - num))
     return cost
 
 min_cost = math.inf
@@ -24,3 +28,5 @@ for i in range(len(tmp_data)):
     min_cost = min(min_cost, move_all_to(tmp_data, i))
 
 print(min_cost)
+
+
