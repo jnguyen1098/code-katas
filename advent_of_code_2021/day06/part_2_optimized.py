96dc9767bc101adcf926a8e1a9d97c10808d68d2
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-06 23:09:38 -0500

    use arr instead of dict

--- advent_of_code_2021/day06/part_2_optimized.py
+++ advent_of_code_2021/day06/part_2_optimized.py
@@ -3,19 +3,22 @@
 from collections import defaultdict, Counter
 
 def advance(data):
-    thing = defaultdict(int)
-    thing[8] = data[0]
-    thing[7] = data[8]
-    thing[6] = data[7]
-    for i in range(0, 7):
-        thing[(i - 1) % 7] += data[i]
+    thing = [0] * 9
+
+    for i in range(9):
+        thing[i] = data[(i + 1) % 9]
+
+    thing[6] += data[0]
     return thing
 
-data = Counter([int(tmp) for tmp in open("example", "r").read().split(",")])
+tmp_data = [int(tmp) for tmp in open("input", "r").read().split(",")]
 
-for i in range(3):
-    print(data)
+data = [0] * 9
+for i in range(len(tmp_data)):
+    data[tmp_data[i]] += 1
+
+for i in range(256):
     data = advance(data)
 
-print(sum(data.values()))
-print(sum(data.values()) == 1686252324092)
+print(sum(data))
+print(sum(data) == 1686252324092)
