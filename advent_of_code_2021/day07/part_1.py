6d7c7f0fc3cc21f3c78a2923df97a9a5347a8f18
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-07 00:03:27 -0500

    boilerplate

--- advent_of_code_2021/day07/part_1.py
+++ advent_of_code_2021/day07/part_1.py
@@ -9,6 +9,16 @@ from bisect import bisect_left, bisect_right
 
 MOD = 1000000007
 
-tmp_data = [int(tmp) for tmp in open("input", "r").read().split(",")]
+#tmp_data = [int(tmp) for tmp in open("input", "r").read().split(",")]
+tmp_data = [int(tmp) for tmp in open("example", "r").read().split(",")]
 
-print(tmp_data)
+def move_all_to(crabs, num):
+    cost = 0
+    return cost
+
+min_cost = math.inf
+
+for i in range(len(tmp_data)):
+    min_cost = min(min_cost, move_all_to(tmp_data, i))
+
+print(min_cost)
