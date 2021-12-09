b405f5ef0442e4414ac0ba6e768b58d86ad76844
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-09 03:26:26 -0500

    idk

--- advent_of_code_2021/day09/part_1.py
+++ advent_of_code_2021/day09/part_1.py
@@ -7,10 +7,26 @@ from math import gcd, floor, sqrt, log
 from collections import defaultdict, deque
 from bisect import bisect_left, bisect_right
 
-MOD = 1000000007
-
 lines = open("input", "r").read().splitlines()
 
-for idx, line in enumerate(lines):
-    print(idx, line)
-print("linecount:", len(lines))
+MOVES = ((-1, 0), (0, -1), (0, 1), (1, 0))
+
+def is_low_point(point, x, y, rows, cols):
+    for MOVE in MOVES:
+        new_x = x + MOVE[0]
+        new_y = y + MOVE[1]
+        if new_x >= 0 and new_x < rows and new_y >= 0 and new_y < cols and int(point[new_x][new_y]) < int(point[x][y]):
+            return False
+    return True
+
+def get_risk(point, x, y):
+    return int(point[x][y]) + 1
+
+risk_sum = 0
+
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        if is_low_point(lines, i, j, len(lines), len(lines[0])):
+            risk_sum += get_risk(lines, i, j)
+
+print(risk_sum)
