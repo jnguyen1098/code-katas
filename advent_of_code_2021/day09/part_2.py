d6403d8b270c9c837f485d4ee80d32e238be2dfc
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-09 04:00:17 -0500

    the number doesnt work

--- advent_of_code_2021/day09/part_2.py
+++ advent_of_code_2021/day09/part_2.py
@@ -24,7 +24,33 @@ def is_low_point(point, x, y, rows, cols):
                 return False
     return True
 
-def get_basin_size(point, x, y, rows, cols, cum):
+def get_basin_size(point, x, y, rows, cols):
+    if point[x][y] == 9:
+        print("something seriuosly went wrong lmoa")
+        exit(1)
+
+    bfs = deque([(x, y)])
+    print(f"creating bfs {bfs}")
+    size = 1
+
+    while bfs:
+        pop_x, pop_y = bfs.popleft()
+        curr_val = point[pop_x][pop_y]
+        print(f"popped {pop_x}, {pop_y}, which has value {curr_val}")
+        point[pop_x][pop_y] = 9
+        for MOVE in MOVES:
+            new_x = pop_x + MOVE[0]
+            new_y = pop_y + MOVE[1]
+            if new_x >= 0 and new_x < rows and new_y >= 0 and new_y < cols:
+                print(f"checking it against {new_x},{new_y} ({point[new_x][new_y]})")
+                if point[new_x][new_y] != 9 and point[new_x][new_y] >= curr_val:
+                    print(new_x, new_y, "passes")
+                    size += 1
+                    bfs.append((new_x, new_y))
+
+    print(f"done. basin size {size}\n")
+    return size
+    """
     if point[x][y] in [-1, 9]:
         return 0
     for MOVE in MOVES:
@@ -36,13 +62,15 @@ def get_basin_size(point, x, y, rows, cols, cum):
                 cum += get_basin_size(point, new_x, new_y, cum + 1)
                 point[new_x][new_y] = tmp
     return 0
+    """
 
 basins = []
 
 for i in range(len(lines)):
     for j in range(len(lines[i])):
         if is_low_point(lines, i, j, len(lines), len(lines[0])):
-            basins.append(get_basin_size(lines, i, j, len(lines), len(lines[0]), 0))
+            basins.append(get_basin_size(lines, i, j, len(lines), len(lines[0])))
 
 basins.sort()
-print(basins.pop() + basins.pop() + basins.pop())
+print(basins)
+print(basins.pop() * basins.pop() * basins.pop())
