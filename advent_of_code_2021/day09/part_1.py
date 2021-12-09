c057deee79dd7c3012b63f2c0ab2f40f41ea6ea8
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-09 03:15:19 -0500

    boilerplate

---
+++ advent_of_code_2021/day09/part_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+
+from math import gcd, floor, sqrt, log
+from collections import defaultdict, deque
+from bisect import bisect_left, bisect_right
+
+MOD = 1000000007
+
+lines = open("input", "r").read().splitlines()
+
+for idx, line in enumerate(lines):
+    print(idx, line)
+print("linecount:", len(lines))
