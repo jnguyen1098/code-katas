8f00959de96dc91828128396b17edc255fe50feb
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-10 00:44:40 -0500

    clean up part_1

--- advent_of_code_2021/day10/part_1.py
+++ advent_of_code_2021/day10/part_1.py
@@ -1,37 +1,14 @@
 #!/usr/bin/env python3
 
-import sys
-import math
-
-from math import gcd, floor, sqrt, log
-from collections import defaultdict, deque
-from bisect import bisect_left, bisect_right
-
-MOD = 1000000007
-
 inputname = "example"
 inputname = "real"
 
-correct = {
-    "(": ")",
-    "[": "]",
-    "{": "}",
-    "<": ">",
-}
-
-_correct = {v: k for k, v in correct.items()}
-
-errorscore = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137,
-}
+inverse = { ")": "(", "]": "[", "}": "{", ">": "<" }
+errorscore = { ")": 3, "]": 57, "}": 1197, ">": 25137 }
 
 lines = open(inputname, "r").read().splitlines()
 
 stack = []
-
 score = 0
 
 for idx, line in enumerate(lines):
@@ -40,8 +17,7 @@ for idx, line in enumerate(lines):
             stack.append(char)
         else:
             popped = stack.pop()
-            if popped != _correct[char]:
-                print("Error", char)
+            if popped != inverse[char]:
                 score += errorscore[char]
-        
+
 print(score)
