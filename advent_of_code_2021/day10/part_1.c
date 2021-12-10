188498524732e6706c67f81c9bf2e332ef4fffb2
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-10 01:17:02 -0500

    clean up part 1 c solution

--- advent_of_code_2021/day10/part_1.c
+++ advent_of_code_2021/day10/part_1.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-#if 0
-# define INPUT "example"
-#else
-# define INPUT "real"
-#endif
-
 #define LEFT(c)((c) == '(' || (c) == '[' || (c) == '{' || (c == '<'))
 
 int stack[BUFSIZ], sp = 0;
@@ -17,7 +11,7 @@ int main(void)
 {
     int score = 0;
     char buf[BUFSIZ];
-    FILE *input = fopen(INPUT, "r");
+    FILE *input = fopen("real", "r");
     while (fgets(buf, BUFSIZ, input)) {
         for (char *s = buf; *s && *s != '\n'; s++) {
             if (LEFT(*s)) {
@@ -31,24 +25,3 @@ int main(void)
     }
     printf("%d\n", score);
 }
-
-/*
-inverse = { ")": "(", "]": "[", "}": "{", ">": "<" }
-errorscore = { ")": 3, "]": 57, "}": 1197, ">": 25137 }
-
-lines = open(inputname, "r").read().splitlines()
-
-stack = []
-score = 0
-
-for idx, line in enumerate(lines):
-    for char in line:
-        if char in "([{<":
-            stack.append(char)
-        else:
-            popped = stack.pop()
-            if popped != inverse[char]:
-                score += errorscore[char]
-
-print(score)
-*/
