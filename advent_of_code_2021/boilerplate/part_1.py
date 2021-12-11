ef48e0274d76500ecb1b83d4399eefe7b44fabcc
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-11 08:05:17 -0500

    create central output

--- advent_of_code_2021/boilerplate/part_1.py
+++ advent_of_code_2021/boilerplate/part_1.py
@@ -6,11 +6,7 @@ sys.path.append("..")
 from ansi import *
 from comp import *
 
-if __name__ == "__main__":
-    inputname = "real"
-    inputname = "example"
-    inputname = "small"
-
+def solve(prob, inputname):
     lines = []
     gen = yield_line(inputname)
 
@@ -20,3 +16,22 @@ if __name__ == "__main__":
     print_arr(lines)
 
     print(f"{len(lines)} lines in the array")
+
+    if prob == 1:
+        return 10
+    elif prob == 2:
+        return 20
+    else:
+        print("Invalid problem code")
+        exit()
+
+if __name__ == "__main__":
+    inputname = "real"
+    inputname = "example"
+    inputname = "small"
+
+    output1 = solve(1, inputname)
+    print(f"Problem 1: {output1}")
+
+    output2 = solve(2, inputname)
+    print(f"Problem 2: {output2}")
