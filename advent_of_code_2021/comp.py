0b9573faac6838cb8c88b25e502cf2ab8013912b
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-11 08:34:02 -0500

    docs

--- advent_of_code_2021/comp.py
+++ advent_of_code_2021/comp.py
@@ -17,6 +17,7 @@ from ansi import *
 MOD = 1000000007
 
 class DIR:
+    """Directions for graphs/grid problems."""
     NW, N, NE = (-1, -1), (-1,  0), (-1,  1)
     W,     E = ( 0,  1), ( 0, -1)
     SW, S, SE = ( 1, -1), ( 1,  0), ( 1,  1)
@@ -27,6 +28,7 @@ class DIR:
     ADJA = (*HORZ, *VERT)
 
 def expect(actual, expected):
+    """Compares two values and spits out equality and message."""
     if actual != expected:
         ret_msg = f"FAIL: Expected {expected} but got {actual}"
         ret_sta = False
@@ -34,6 +36,7 @@ def expect(actual, expected):
     return True, "PASS"
 
 def point(old, new, rows, cols):
+    """Translates a point given a displacement tuple."""
     new_x = old[0] + new[0]
     new_y = old[1] + new[1]
     if new_x >= 0 and new_x < rows and new_y >= 0 and new_y < cols:
@@ -41,6 +44,7 @@ def point(old, new, rows, cols):
     return None
 
 def print_arr(arr, sep=""):
+    """Array printing but prettier."""
     print('\n'.join([sep.join([str(cell) for cell in row]) for row in arr]))
 
 revdict = lambda dt: {v: k for k, v in dt.items()}
