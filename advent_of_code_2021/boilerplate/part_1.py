742e97257a08bbb2590317ff4117adbc4c24e091
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-11 06:24:29 -0500

    boilerplate update

--- advent_of_code_2021/boilerplate/part_1.py
+++ advent_of_code_2021/boilerplate/part_1.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
+import copy
 import sys
 import math
 
-from math import gcd, floor, sqrt, log
+sys.path.append("..")
+
+from math import ceil, floor, inf, log, sqrt
 from collections import defaultdict, deque
-from bisect import bisect_left, bisect_right
+
+# EFFECTS: (bold|dim|undr|blnk|rev|hidn)
+# COLOURS: (l|b)?(red|grn|yel|blu|mag|cya|wht)
+from ansi import *
 
 MOD = 1000000007
+
+def rev_dict(input_dict):
+    return {v: k for k, v in input_dict.items()}
+
+def print_arr(arr):
+    print('\n'.join([' '.join([str(cell) for cell in row]) for row in arr]))
+
+introw = lambda text: list(map(int, text.strip().split()))
+intgrid = lambda text: list(map(int, [char for char in text.strip()]))
 
 inputname = "real"
 inputname = "example"
