e2311b543d57ddc31ef2946e33a6102561e2323f
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-14 22:01:26 -0500

    clean up

--- advent_of_code_2021/day14/efficient.py
+++ advent_of_code_2021/day14/efficient.py
@@ -19,43 +19,26 @@ def solve(prob, inputname):
     for line in gen:
         lines.append(parse(r"(\w)(\w) -> (\w)", line))
 
-    recipes = {}
-
-    for line in lines:
-        recipes[f"{line[0]}{line[1]}"] = line[2]
+    recipes = {f"{line[0]}{line[1]}": line[2] for line in lines}
 
     tiles = defaultdict(int)
     freqs = Counter(template)
 
     for i in range(len(template) - 1):
-        l, r = template[i], template[i + 1]
-        tiles[f"{l}{r}"] += 1
+        tiles[f"{template[i]}{template[i + 1]}"] += 1
 
     times = 10 if prob == 1 else 40
-
-    def print_dict(my_dict):
-        for key, value in my_dict.items():
-            if value > 0:
-                print(f"{key} = {value}")
 
     for i in range(times):
         reqs = copy.deepcopy(tiles)
         for key, value in reqs.items():
             tiles[key] -= value
-            if tiles[key] == 0:
-                tiles.pop(key)
-            new = recipes[key]
-            freqs[new] += value
-            nl, nr = f"{key[0]}{new}", f"{new}{key[1]}"
-            tiles[nl] += value
-            tiles[nr] += value
+            result = recipes[key]
+            freqs[result] += value
+            tiles[f"{key[0]}{result}"] += value
+            tiles[f"{result}{key[1]}"] += value
 
-    counter = Counter(freqs)
-    listcnt = sorted([(freq, letter) for letter, freq in counter.items()])
-    return int(listcnt.pop(-1)[0]) - int(listcnt.pop(0)[0])
-
-    print(listcnt)
-    print(recipes)
+    return int(freqs.most_common()[0][1]) - int(freqs.most_common()[-1][1])
 
 if __name__ == "__main__":
     inputs = ["example", "real"]
