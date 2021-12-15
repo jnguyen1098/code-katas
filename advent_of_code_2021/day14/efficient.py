db3fdfdb1da5def3618b69733dff48db0012e45e
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-14 21:50:18 -0500

    part 2

--- advent_of_code_2021/day14/efficient.py
+++ advent_of_code_2021/day14/efficient.py
@@ -6,6 +6,8 @@ sys.path.append("..")
 from ansi import *
 from comp import *
 
+from collections import Counter
+
 def solve(prob, inputname):
     lines = []
     gen = yield_line(inputname)
@@ -22,67 +24,36 @@ def solve(prob, inputname):
     for line in lines:
         recipes[f"{line[0]}{line[1]}"] = line[2]
 
-    counts = defaultdict(int)
+    tiles = defaultdict(int)
+    freqs = Counter(template)
 
     for i in range(len(template) - 1):
         l, r = template[i], template[i + 1]
-        counts[f"{l}{r}"] += 1
+        tiles[f"{l}{r}"] += 1
 
     times = 10 if prob == 1 else 40
 
-    from collections import Counter
-    leftovers = defaultdict(int)
-    letters = defaultdict(int)
+    def print_dict(my_dict):
+        for key, value in my_dict.items():
+            if value > 0:
+                print(f"{key} = {value}")
 
-    for letter in template:
-        letters[letter] += 1
-
-    import time
-    record = time.time()
     for i in range(times):
-        now = time.time()
-        print("\n", now - record)
-        record = now
-        keys = Counter(list(counts.keys()))
-        keys.update(leftovers)
-        print(f"{i} = {sum(keys.values())}")
-#        print("iterating over", list(counts.keys()), "and", leftovers[:], f"len {len(keys)}")
-        leftovers.clear()
-
-        for key, count in keys.items():
-            pat = f"{key[0]}{key[1]}"
-            counts[pat] -= count
-            if counts[pat] < 1:
-                counts.pop(pat)
-
-        for key, count in keys.items():
-            pat = f"{key[0]}{key[1]}"
-            new = recipes[pat]
+        reqs = copy.deepcopy(tiles)
+        for key, value in reqs.items():
+            tiles[key] -= value
+            if tiles[key] == 0:
+                tiles.pop(key)
+            new = recipes[key]
+            freqs[new] += value
             nl, nr = f"{key[0]}{new}", f"{new}{key[1]}"
-            for i in range(count):
-#                print(f"tile: {pat} -> {new} : {nl} & {nr}")
-    
-                if nl in counts:
-#                    print(f"duplicate {nl}. appending.")
-                    leftovers[nl] += 1
-                else:
-                    counts[nl] = 1
-                    
-                    
-                if nr in counts:
-#                    print(f"duplicate {nr}. appending.")
-                    leftovers[nr] += 1
-                else:
-                    counts[nr] = 1
-    
-            counts[nl] += count - 1
-            counts[nr] += count - 1
-#                print("adding", new)
-            letters[new] += count
+            tiles[nl] += value
+            tiles[nr] += value
 
-    print("fuck off", letters)
-    print(counts)
-    counter = Counter(letters)
+    print("\ndone everything")
+    print("letter frequencies", freqs)
+    print("tile counts", tiles)
+    counter = Counter(freqs)
     listcnt = sorted([(freq, letter) for letter, freq in counter.items()])
     return int(listcnt.pop(-1)[0]) - int(listcnt.pop(0)[0])
 
