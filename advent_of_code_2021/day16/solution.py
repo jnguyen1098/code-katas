5b9dcd7df0d9b374becbec08b9f0cfa46dba3849
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-15 23:58:29 -0500

    day16 boil

---
+++ advent_of_code_2021/day16/solution.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append("..")
+
+from ansi import *
+from comp import *
+
+def solve(prob, inputname):
+    lines = []
+    gen = yield_line(inputname)
+
+    for line in gen:
+        lines.append(line)
+
+    print_arr(lines)
+
+    print(f"{len(lines)} lines in the array")
+
+    if prob == 1:
+        return 1
+    elif prob == 2:
+        return 2
+    else:
+        print("Invalid problem code")
+        exit()
+
+if __name__ == "__main__":
+    inputs = ["small", "example", "real"]
+    expcts = [[10, 20, 30], [40, 50, 60]]
+    shortc = True
+
+    for idx, part in enumerate(expcts):
+        for filename, expected in zip(inputs, part):
+            print(cya(rev(f"Filename: {filename}")))
+            output = solve(idx + 1, filename)
+            passed, msg = expect(output, expected)
+            result = rev(grn("PASS") if passed else red("FAIL"))
+            print(f"Part {idx + 1}: {output} {grn(msg) if passed else red(msg)}\n")
+            if not passed and shortc: exit()
+        print("\n" * 2)
