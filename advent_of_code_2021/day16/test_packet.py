d885e032f968991557bba01f477d24335b04c1f9
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-16 10:55:25 -0500

    complex test

--- advent_of_code_2021/day16/test_packet.py
+++ advent_of_code_2021/day16/test_packet.py
@@ -5,10 +5,18 @@ from packet import *
 
 class TestAllMethods(unittest.TestCase):
 
-    def test_stream_to_hex_array(self):
+    def test_stream_to_hex_array_simple(self):
         stream = "00FF"
         result = stream_to_hex_array(stream)
         self.assertEqual(result, ["00", "FF"])
+
+    def test_stream_to_hex_array_complex(self):
+        stream_arr = []
+        for i in range(0x00, 0xFF + 1):
+            stream_arr.append(hex(i)[2:].upper().zfill(2))
+        stream = "".join(stream_arr)
+        result = stream_to_hex_array(stream)
+        self.assertEqual(result, stream_arr)
 
 if __name__ == "__main__":
     unittest.main()
