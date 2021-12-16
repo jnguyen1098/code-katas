41a67c4f5d7e508d65b42665c8c7a16fb53fda17
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-16 11:32:03 -0500

    linting

--- advent_of_code_2021/day16/packet.py
+++ advent_of_code_2021/day16/packet.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
+"""Packet module."""
 
 import re
+from typing import List
 
-def stream_to_hex_array(stream):
+
+class PacketRuntimeException(Exception):
+    """Just a fancy exception name."""
+
+
+def die(message: str) -> None:
+    """Raise message and then die."""
+    raise PacketRuntimeException(message)
+
+
+def stream_to_hex_array(stream: str) -> List[str]:
+    """Convert stream string to array of padded hex."""
     result = re.findall(r"[0-9A-F]{2}", stream)
-    assert len(stream) // 2 == len(result),\
-    f"streamlen {stream} ({len(stream)}) != result len {result} ({len(result)})"
+    assert len(stream) // 2 == len(
+        result
+    ), f"streamlen {stream} ({len(stream)}) != result len {result} ({len(result)})"
     return result
 
-def number_to_binary(num, pad=0):
+
+def number_to_binary(num: int, pad: int = 0) -> str:
+    """Convert integer to padded binary string."""
     return format(num, "b").zfill(pad)
 
-def read(string, l, r):
-    assert l <= r, f"invalid read(), l ({l}) > r ({r})"
-    return r - l + 1
+
+def read(string: str, left: int, right: int) -> str:
+    """Read string on inclusive indices [left, right], returning read bytes."""
+    if left <= right:
+        die(f"invalid read(), l ({left}) > r ({right})")
+    return string[left : right + 1]
--- advent_of_code_2021/day16/test_packet.py
+++ advent_of_code_2021/day16/test_packet.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python3
+"""Unit tests."""
 
 import unittest
-from packet import *
+
+import packet
+
 
 class TestAllMethods(unittest.TestCase):
+    """Test all methods class."""
 
-    def test_stream_to_hex_array_simple(self):
+    def test_die(self) -> None:
+        """Test the die function."""
+        with self.assertRaises(packet.PacketRuntimeException):
+            packet.die("this should raise something")
+
+    def test_stream_to_hex_array_simple(self) -> None:
+        """Testing the hex array conversion function."""
         stream = "00FF"
-        result = stream_to_hex_array(stream)
+        result = packet.stream_to_hex_array(stream)
         self.assertEqual(result, ["00", "FF"])
 
-    def test_stream_to_hex_array_complex(self):
+    def test_stream_to_hex_array_complex(self) -> None:
+        """Testing the hex array conversion function."""
         stream_arr = []
         for i in range(0x00, 0xFF + 1):
             stream_arr.append(hex(i)[2:].upper().zfill(2))
         stream = "".join(stream_arr)
-        result = stream_to_hex_array(stream)
+        result = packet.stream_to_hex_array(stream)
         self.assertEqual(result, stream_arr)
 
-    def test_number_to_binary(self):
-        self.assertEqual(number_to_binary(12, 8), "00001100")
-        self.assertEqual(number_to_binary(12), "1100")
-        self.assertEqual(number_to_binary(12, 0), "1100")
-        self.assertEqual(number_to_binary(255, 0), "11111111")
-        self.assertEqual(number_to_binary(0, 0), "0")
-        self.assertEqual(number_to_binary(0, 8), "00000000")
+    def test_number_to_binary(self) -> None:
+        """Test number to binary."""
+        self.assertEqual(packet.number_to_binary(12, 8), "00001100")
+        self.assertEqual(packet.number_to_binary(12), "1100")
+        self.assertEqual(packet.number_to_binary(12, 0), "1100")
+        self.assertEqual(packet.number_to_binary(255, 0), "11111111")
+        self.assertEqual(packet.number_to_binary(0, 0), "0")
+        self.assertEqual(packet.number_to_binary(0, 8), "00000000")
+
 
 if __name__ == "__main__":
     unittest.main()
