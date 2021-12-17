d7a3fcabcd7794de9b74c1db79f56db463c2740d
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-17 01:26:49 -0500

    massive shortening/cleanup

--- advent_of_code_2021/day17/solution.py
+++ advent_of_code_2021/day17/solution.py
@@ -6,37 +6,11 @@ sys.path.append("..")
 from ansi import *
 from comp import *
 
-class Point:
-    def __init__(self, x_vel, y_vel):
-        self.x = 0
-        self.y = 0
-        self.x_vel = x_vel
-        self.y_vel = y_vel
-    def step(self):
-        self.x += self.x_vel
-        self.y += self.y_vel
-        if self.x_vel > 0:
-            self.x_vel -= 1
-        elif self.x_vel < 0:
-            self.x_vel += 1
-        self.y_vel -= 1
-    def __str__(self):
-        return "\n".join(
-            [f"x={self.x}", f"y={self.y}", f"x_vel={self.x_vel}", f"y_vel={self.y_vel}", ""]
-        )
-
-def within(x, y, x_l, x_r, y_l, y_r):
-    return x_l <= x <= x_r and y_l <= y <= y_r
-
 def solve(prob, inputname):
-    lines = []
-    gen = yield_line(inputname)
+    line = parse(r"target area: x=([^,]+), y=(.*)", open(inputname).read())
 
-    for line in gen:
-        lines.append(parse(r"target area: x=([^,]+), y=(.*)", line))
-
-    x_l, x_r = lines[0][0].split("..")
-    y_l, y_r = lines[0][1].split("..")
+    x_l, x_r = line[0].split("..")
+    y_l, y_r = line[1].split("..")
 
     global_highest_y = -math.inf
     reached = False
@@ -45,18 +19,22 @@ def solve(prob, inputname):
 
     for i in range(300):
         for j in range(-300, 300):
-            point = Point(i, j)
+            x, y = 0, 0
+            x_v, y_v = i, j
             highest_y = -math.inf
         
             while True:
-                point.step()
-                highest_y = max(highest_y, point.y)
-                if within(point.x, point.y, int(x_l), int(x_r), int(y_l), int(y_r)):
+                x += x_v
+                y += y_v
+                x_v += 1 if x_v < 0 else -1 if x_v > 0 else 0
+                y_v -= 1
+                highest_y = max(highest_y, y)
+                if int(x_l) <= x <= int(x_r) and int(y_l) <= y <= int(y_r):
                     reached = True
                     global_highest_y = max(global_highest_y, highest_y)
                     vals += 1
                     break
-                elif point.x > int(x_r) or point.y < int(y_l):
+                elif x > int(x_r) or y < int(y_l):
                     break
 
     if prob == 1:
