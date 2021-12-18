22d3552ed5169d988a93e41081e51bf4c097098b
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-18 00:32:45 -0500

    another test

--- advent_of_code_2021/day18/test_solution.py
+++ advent_of_code_2021/day18/test_solution.py
@@ -8,6 +8,7 @@ class TestAll(unittest.TestCase):
     def test_str_to_pair(self):
         self.assertEqual(str_to_pair("[1,2]"), [1, 2])
         self.assertEqual(str_to_pair("[1,[2,3]]"), [1, [2,3]])
+        self.assertEqual(str_to_pair("[[4,[[[]]],6],[2,3]]"), [[4,[[[]]],6],[2,3]])
 
 if __name__ == "__main__":
     unittest.main()
