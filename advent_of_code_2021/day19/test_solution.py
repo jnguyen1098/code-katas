ee8d6fd9b986e8f6fe9494ccab5e4b94d3c7d1b5
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-19 00:33:55 -0500

    add example parsing to test_solve

--- advent_of_code_2021/day19/test_solution.py
+++ advent_of_code_2021/day19/test_solution.py
@@ -7,7 +7,9 @@ class TestAll(unittest.TestCase):
 
     def test_solve(self):
         self.assertEqual(solve(1, "input"), 1)
+        self.assertEqual(solve(1, "example"), 1)
         self.assertEqual(solve(2, "input"), 2)
+        self.assertEqual(solve(2, "example"), 2)
 
 if __name__ == "__main__":
     unittest.main()
