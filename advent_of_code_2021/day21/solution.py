20d805a8416eaaf50eb072ea4062103daf899ee2
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-21 15:19:34 -0500

    rearrange some stuff

--- advent_of_code_2021/day21/solution.py
+++ advent_of_code_2021/day21/solution.py
@@ -9,6 +9,7 @@ from comp import *
 from collections import Counter
 
 ONE, TWO = (0, 1)
+ROLL_DISTRIBUTION = {3: 1,  4: 3,  5: 6,  6: 7,  7: 6,  8: 3,  9: 1}
 
 def get_next_position(curr, advance):
     return (curr + advance - 1) % 10 + 1
@@ -24,7 +25,7 @@ def decode(serialized):
     turn = spl[2]
     return int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1]), int(turn)
 
-def normal_game(player_1, player_2):
+def get_losing_score(player_1, player_2):
     position_of_player = [player_1, player_2]
     score_of_player = [0, 0]
     curr_die_val = 0
@@ -43,49 +44,44 @@ def normal_game(player_1, player_2):
 
     return rolls * min(score_of_player)
 
+def get_universal_outcomes(state, win_cache={}):
+    if memo := win_cache.get(state):
+        return memo
+    p1, s1, p2, s2, turn = decode(state)
+    p1_wins = 0
+    p2_wins = 0
+    for roll, freq in ROLL_DISTRIBUTION.items():
+        if turn == 1:
+            new_p1 = get_next_position(p1, roll)
+            new_s1 = s1 + new_p1
+            new_state = encode(new_p1, new_s1, p2, s2, 2)
+            if new_s1 >= 21:
+                p1_wins += freq
+            else:
+                incr1, incr2 = get_universal_outcomes(new_state)
+                p1_wins += incr1 * freq
+                p2_wins += incr2 * freq
+        elif turn == 2:
+            new_p2 = get_next_position(p2, roll)
+            new_s2 = s2 + new_p2
+            new_state = encode(p1, s1, new_p2, new_s2, 1)
+            if new_s2 >= 21:
+                p2_wins += freq
+            else:
+                incr1, incr2 = get_universal_outcomes(new_state)
+                p1_wins += incr1 * freq
+                p2_wins += incr2 * freq
+    win_cache[state] = (p1_wins, p2_wins)
+    return win_cache[state]
+
 def solve(prob, inputname):
 
     player_1, player_2 = [int(line[line.index(":") + 2:]) for line in yield_line(inputname)]
 
-    if prob == 1: return normal_game(player_1, player_2)
+    if prob == 1: return get_losing_score(player_1, player_2)
 
     if prob == 2:
-
-        rolls = {3: 1,  4: 3,  5: 6,  6: 7,  7: 6,  8: 3,  9: 1}
-    
-        win_cache = {}
-
-        def get_wins(state, level):
-            if memo := win_cache.get(state):
-                return memo
-            p1, s1, p2, s2, turn = decode(state)
-            p1_wins = 0
-            p2_wins = 0
-            for roll, freq in rolls.items():
-                if turn == 1:
-                    new_p1 = get_next_position(p1, roll)
-                    new_s1 = s1 + new_p1
-                    new_state = encode(new_p1, new_s1, p2, s2, 2)
-                    if new_s1 >= 21:
-                        p1_wins += freq
-                    else:
-                        incr1, incr2 = get_wins(new_state, level + 1)
-                        p1_wins += incr1 * freq
-                        p2_wins += incr2 * freq
-                elif turn == 2:
-                    new_p2 = get_next_position(p2, roll)
-                    new_s2 = s2 + new_p2
-                    new_state = encode(p1, s1, new_p2, new_s2, 1)
-                    if new_s2 >= 21:
-                        p2_wins += freq
-                    else:
-                        incr1, incr2 = get_wins(new_state, level + 1)
-                        p1_wins += incr1 * freq
-                        p2_wins += incr2 * freq
-            win_cache[state] = (p1_wins, p2_wins)
-            return win_cache[state]
-
-        p1_wins, p2_wins = get_wins(f"{player_1},0|{player_2},0|1", level=1)
+        p1_wins, p2_wins = get_universal_outcomes(f"{player_1},0|{player_2},0|1", {})
         print(f"Player 1 won {p1_wins} times")
         print(f"Player 2 won {p2_wins} times")
 
