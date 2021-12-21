4ed4803407dae86a8d112af4d04d2e55355ba38a
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-21 14:28:22 -0500

    remove debuggin print statements

--- advent_of_code_2021/day21/solution.py
+++ advent_of_code_2021/day21/solution.py
@@ -87,70 +87,22 @@ def solve(prob, inputname):
                  6, 7, 8,
                  7, 8, 9]
     
-        """
-        print(rolls)
-        roll_freqs = Counter(rolls)
-        for outcome, freq in roll_freqs.items():
-            print(f"roll  {outcome}     occurs {freq} times")
-
-        print(f"player 1 starts {pos1}")
-        print(f"player 2 starts {pos2}")
-
-        limit = 6
-
-        print("player 1 wins", win1, "times")
-        print("player 2 wins", win2, "times")
-        """
-
-        """
-        Roll 1, then 2, then 3
-        When score is >= 10, player wins
-        """
         tree = {}
 
-        def print_state(state):
-            position_1, score_1, position_2, score_2, turn = decode(state)
-            #print(f"{state} is {position_1=}  {score_1=}  {position_2=}  {score_2=}  {turn=}")
-            print(state)
-
-        def indent(level):
-            return " " * 4 * level
-
-        print()
         def get_wins(state, level):
-            """
-            print(indent(level - 1), end="")
-            print_state(state)
-            """
             if memo := tree.get(state):
-                """
-                print(f"{indent(level)} This position was memoized. Returning {memo=}")
-                """
                 return memo
             p1, s1, p2, s2, turn = decode(state)
             p1_wins = 0
             p2_wins = 0
-            """
-            for roll in [1, 2, 3]:
-            """
             for roll in rolls:
                 if turn == 1:
                     new_p1 = get_position(p1, roll)
                     new_s1 = s1 + new_p1
                     new_state = encode(new_p1, new_s1, p2, s2, 2)
-                    """
-                    print(f"{indent(level)}P1 rolled {roll}. Pos {p1} -> {new_p1}. Score {s1} -> {new_s1}")
-                    print(f"{indent(level + 1)}New state is {new_state}")
-                    """
                     if new_s1 >= 21:
-                        """
-                        print(f"{indent(level + 2)}Because p1 has score {new_s1}, they win this node")
-                        """
                         p1_wins += 1
                     else:
-                        """
-                        print(f"{indent(level + 2)}Nobody wins yet. Recursive request for {new_state}\n")
-                        """
                         incr1, incr2 = get_wins(new_state, level + 1)
                         p1_wins += incr1
                         p2_wins += incr2
@@ -158,28 +110,12 @@ def solve(prob, inputname):
                     new_p2 = get_position(p2, roll)
                     new_s2 = s2 + new_p2
                     new_state = encode(p1, s1, new_p2, new_s2, 1)
-                    """
-                    print(f"{indent(level)}P2 rolled {roll}. Pos {p2} -> {new_p2}. Score {s2} -> {new_s2}")
-                    print(f"{indent(level + 1)}New state is {new_state}")
-                    """
                     if new_s2 >= 21:
-                        """
-                        print(f"{indent(level + 2)}Because p2 has score {new_s2}, they win this node")
-                        """
                         p2_wins += 1
                     else:
-                        """
-                        print(f"{indent(level + 3)}Nobody wins yet. Recursive request for {new_state}\n")
-                        """
                         incr1, incr2 = get_wins(new_state, level + 1)
                         p1_wins += incr1
                         p2_wins += incr2
-                else:
-                    print("Impossible turn", turn)
-                    exit(1)
-                """
-                print()
-                """
             tree[state] = (p1_wins, p2_wins)
             return tree[state]
 
