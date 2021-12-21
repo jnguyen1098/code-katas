ce58be3e20a82fd595f88e9d33a755f36e09ac38
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-21 15:11:27 -0500

    optimize using dictionary

--- advent_of_code_2021/day21/solution.py
+++ advent_of_code_2021/day21/solution.py
@@ -51,23 +51,7 @@ def solve(prob, inputname):
 
     if prob == 2:
 
-        pos1 = player_1
-        pos2 = player_2
-        sco1 = 0
-        sco2 = 0
-        turn = 1
-        win1 = 0
-        win2 = 0
-
-        rolls = [3, 4, 5,
-                 4, 5, 6,
-                 5, 6, 7,
-                 4, 5, 6,
-                 5, 6, 7,
-                 6, 7, 8,
-                 5, 6, 7,
-                 6, 7, 8,
-                 7, 8, 9]
+        rolls = {3: 1,  4: 3,  5: 6,  6: 7,  7: 6,  8: 3,  9: 1}
     
         tree = {}
 
@@ -77,27 +61,27 @@ def solve(prob, inputname):
             p1, s1, p2, s2, turn = decode(state)
             p1_wins = 0
             p2_wins = 0
-            for roll in rolls:
+            for roll, freq in rolls.items():
                 if turn == 1:
                     new_p1 = get_next_position(p1, roll)
                     new_s1 = s1 + new_p1
                     new_state = encode(new_p1, new_s1, p2, s2, 2)
                     if new_s1 >= 21:
-                        p1_wins += 1
+                        p1_wins += freq
                     else:
                         incr1, incr2 = get_wins(new_state, level + 1)
-                        p1_wins += incr1
-                        p2_wins += incr2
+                        p1_wins += incr1 * freq
+                        p2_wins += incr2 * freq
                 elif turn == 2:
                     new_p2 = get_next_position(p2, roll)
                     new_s2 = s2 + new_p2
                     new_state = encode(p1, s1, new_p2, new_s2, 1)
                     if new_s2 >= 21:
-                        p2_wins += 1
+                        p2_wins += freq
                     else:
                         incr1, incr2 = get_wins(new_state, level + 1)
-                        p1_wins += incr1
-                        p2_wins += incr2
+                        p1_wins += incr1 * freq
+                        p2_wins += incr2 * freq
             tree[state] = (p1_wins, p2_wins)
             return tree[state]
 
