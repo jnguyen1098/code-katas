50f59d21a91b2bec107577eea37ac79278cde7ba
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-21 00:27:03 -0500

    input for example part 2

--- advent_of_code_2021/day21/test_solution.py
+++ advent_of_code_2021/day21/test_solution.py
@@ -15,7 +15,7 @@ class TestAll(unittest.TestCase):
     def test_solve(self):
         self.assertEqual(solve(1, "example"), 739785)
         self.assertEqual(solve(1, "input"), 752247)
-        self.assertEqual(solve(2, "example"), 2)
+        self.assertEqual(solve(2, "example"), 444356092776315)
         self.assertEqual(solve(2, "input"), 2)
 
 if __name__ == "__main__":
