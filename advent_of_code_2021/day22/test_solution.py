c2c927c61036d2f6d117b5f3a22325ffd07270e7
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-22 00:01:29 -0500

    failfast

--- advent_of_code_2021/day22/test_solution.py
+++ advent_of_code_2021/day22/test_solution.py
@@ -6,8 +6,9 @@ from solution import *
 class TestAll(unittest.TestCase):
 
     def test_solve(self):
-        self.assertEqual(solve(1, "input"), 1)
-        self.assertEqual(solve(2, "input"), 2)
+        self.assertEqual(solve(1, "small"), -1)
+        self.assertEqual(solve(1, "example"), -1)
+        self.assertEqual(solve(1, "input"), -1)
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(failfast=True)
