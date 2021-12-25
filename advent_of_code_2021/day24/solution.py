60a0723df9a617cc4b82c2629a46f115b6af8c76
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-25 13:47:58 -0500

    chunk4

--- advent_of_code_2021/day24/solution.py
+++ advent_of_code_2021/day24/solution.py
@@ -59,15 +59,7 @@ def chunk3(w, z, x=0, y=0):
     return w, 1, w + 5, (z // 26 * 26) + (w + 5)
 
 def chunk4(w, z, x=0, y=0):
-    x = (z % 26) + 11
-    x = int(w != x)
-    y = 25 * x + 1
-    z *= y
-    y = w
-    y += 6
-    y *= x
-    z += y
-    return w, x, y, z
+    return w, 1, w + 6, (z * 26) + (w + 6)
 
 def parse_instructions(inputname):
     instructions = []
