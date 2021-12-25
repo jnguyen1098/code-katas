c3e8f11a8a09a319f8c4606df19105296bd3a0f9
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-24 23:30:38 -0500

    parsing instructions

--- advent_of_code_2021/day24/solution.py
+++ advent_of_code_2021/day24/solution.py
@@ -5,15 +5,29 @@ sys.path.append("..")
 
 from ansi import *
 from comp import *
+from dataclasses import dataclass
+
+@dataclass
+class Instruction:
+    opcode: str
+    lop: str
+    rop: str
+
+INSTRUCTIONS = set(["inp", "add", "sub", "mul", "div"])
 
 def solve(prob, inputname):
     lines = []
     gen = yield_line(inputname)
 
     for line in gen:
-        lines.append(line)
+        split = line.split()
+        if len(split) == 3:
+            lines.append(Instruction(split[0], split[1], split[2]))
+        else:
+            lines.append(Instruction(split[0], split[1], ""))
 
-    print_arr(lines)
+    for line in lines:
+        print(line)
 
     print(f"{len(lines)} in the array")
 
