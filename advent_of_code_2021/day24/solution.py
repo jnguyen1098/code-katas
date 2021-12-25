9836415fbedc0424da6d1dc4b34d0f49d757ae77
jnguyen1098 <jnguyen1098@gmail.com>
2021-12-24 23:45:00 -0500

    some parsing

--- advent_of_code_2021/day24/solution.py
+++ advent_of_code_2021/day24/solution.py
@@ -5,31 +5,58 @@ sys.path.append("..")
 
 from ansi import *
 from comp import *
-from dataclasses import dataclass
-
-@dataclass
-class Instruction:
-    opcode: str
-    lop: str
-    rop: str
 
 INSTRUCTIONS = set(["inp", "add", "sub", "mul", "div"])
 
+class Instruction:
+    def __init__(self, opcode, lop, rop):
+        self.opcode = opcode
+        self.lop = lop
+        if rop.isalpha():
+            self.rop = rop
+        elif rop == "":
+            self.rop = None
+        else:
+            self.rop = int(rop)
+    def __str__(self):
+        return f"{self.opcode} {self.lop} {self.rop if self.rop else ''}"
+
+def get_char(string):
+    for char in string:
+        yield char
+
+def simulate(instructions, input_stream):
+    stream = get_char(input_stream)
+    reg = {"w": 0, "x": 0, "y": 0, "z": 0}
+    for inst in instructions:
+        match inst.opcode:
+            case "inp":
+                reg["w"] = int(next(stream))
+            case "add":
+                print("add")
+#reg[inst.lop] = inst.rop
+            case "sub":
+                print("subtract")
+            case "mul":
+                print("multiply")
+            case "div":
+                print("divide")
+
 def solve(prob, inputname):
-    lines = []
+    instructions = []
     gen = yield_line(inputname)
 
     for line in gen:
         split = line.split()
         if len(split) == 3:
-            lines.append(Instruction(split[0], split[1], split[2]))
+            instructions.append(Instruction(split[0], split[1], split[2]))
         else:
-            lines.append(Instruction(split[0], split[1], ""))
+            instructions.append(Instruction(split[0], split[1], ""))
 
-    for line in lines:
+    for line in instructions:
         print(line)
 
-    print(f"{len(lines)} in the array")
+    simulate(instructions, "1234567890")
 
     if prob == 1:
         return 1
