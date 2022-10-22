f7284c62533c7d301bc0b85be04a499b0182db24
Jason Nguyen <jnguyen1098@gmail.com>
2022-10-22 05:32:02 -0400

    hashing

--- matt/words.py
+++ matt/words.py
@@ -28,6 +28,26 @@ def read_all_words(filename):
             words.append(line)
     return words
 
+def sign_word(word):
+    return "".join(sorted(list(word)))
+
+def get_signatures(words):
+    signatures = set()
+
+    for word in words:
+        signatures.add((sign_word(word), word))
+
+    return signatures
+
+def execute_query(filename):
+    answer = 0
+
+    words = read_all_words(filename)
+    five_letter_words = [word for word in words if len(word) == 5]
+    signatures = get_signatures(five_letter_words)
+
+    return answer
+
 def test_read_all_words_correct_length():
     global level
     level += 1
@@ -35,11 +55,54 @@ def test_read_all_words_correct_length():
     assert_that("the right number of words is read", len(words), 370105)
     level -= 1
 
+def test_sign_word():
+    global level
+    level += 1
+
+    assert_that(
+        "the word boob is signed correctly",
+        sign_word("boob"),
+        "bboo"
+    )
+
+    level -= 1
+
+def test_get_signatures():
+    global level
+    level += 1
+
+    assert_that(
+        "the set ['food', 'cat', 'oyster', 'ooooo'] is signed correctly",
+        get_signatures(["food", "cat", "oyster", "ooooo"]),
+        set([("dfoo", "food"), ("act", "cat"), ("eorsty", "oyster"), ("ooooo", "ooooo")])
+    )
+
+
+    level -= 1
+
+def test_correct_answer():
+    global level
+    level += 1
+
+    start = time.time()
+    answer = execute_query(FILENAME)
+    end = time.time()
+    assert_that("the original answer in the video is given", answer, 538)
+
+    prindent(f"Query executed in {end - start} seconds")
+
+    level -= 1
+
 def run_all_tests():
     global level
     level += 1
     prindent("Running all tests")
+
     test_read_all_words_correct_length()
+    test_sign_word()
+    test_get_signatures()
+    test_correct_answer()
+
     prindent("Done running all tests")
     level -= 1
 
