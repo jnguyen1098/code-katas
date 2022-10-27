de911aab8df4fed9f9d3b1b237a461207d3ecba8
Jason Nguyen <jnguyen1098@gmail.com>
2022-10-26 23:41:20 -0400

    add 2020 folder

---
+++ advent_of_code_2020/boilerplate/test_solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import unittest
+from solution import *
+
+class TestAll(unittest.TestCase):
+
+    def test_solve(self):
+        self.assertEqual(solve(1, "input"), 1)
+        self.assertEqual(solve(2, "input"), 2)
+
+if __name__ == "__main__":
+    unittest.main(failfast=True)
---
+++ advent_of_code_2020/comp.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import copy
+import sys
+import math
+import re
+
+sys.path.append("..")
+sys.setrecursionlimit(100000000)
+
+from math import ceil, floor, inf, log, sqrt
+from collections import defaultdict, deque
+
+# EFFECTS: (bold|dim|undr|blnk|rev|hidn)
+# COLOURS: (l|b)?(red|grn|yel|blu|mag|cya|wht)
+from ansi import *
+
+MOD = 1000000007
+
+class Pointer:
+    def __init__(self, data=None): self.data = data
+
+class DIR:
+    """Directions for graphs/grid problems."""
+    NW, N, NE = (-1, -1), (-1,  0), (-1,  1)
+    W,     E  = ( 0, -1),           ( 0,  1)
+    SW, S, SE = ( 1, -1), ( 1,  0), ( 1,  1)
+    DIAG = (NW, NE, SE, SW)
+    HORZ = (E, W)
+    VERT = (N, S)
+    SURR = (*HORZ, *VERT, *DIAG)
+    ADJA = (*HORZ, *VERT)
+
+def expect(actual, expected):
+    """Compares two values and spits out equality and message."""
+    if actual != expected:
+        ret_msg = f"FAIL: Expected {expected} but got {actual}"
+        ret_sta = False
+        return ret_sta, ret_msg
+    return True, "PASS"
+
+def get_point(old, new, rows, cols):
+    """Translates a point given a displacement tuple."""
+    new_x = old[0] + new[0]
+    new_y = old[1] + new[1]
+    if new_x >= 0 and new_x < rows and new_y >= 0 and new_y < cols:
+        return (new_x, new_y)
+    return None
+
+def print_arr(arr, sep=""):
+    """Array printing but prettier."""
+    print('\n'.join([sep.join([str(cell) for cell in row]) for row in arr]))
+
+revdict = lambda dt: {v: k for k, v in dt.items()}
+
+strsep = lambda text, sep=None: list(text.strip().split(sep))
+intsep  = lambda text, sep=None: list(map(int, text.strip().split(sep)))
+intgrid = lambda text: list(map(int, [char for char in text.strip()]))
+parse = lambda pattern, text: re.match(pattern, text).groups()
+
+def yield_line(filename):
+    with open(filename, "r") as file_p:
+        for line in file_p.read().splitlines():
+            yield line
