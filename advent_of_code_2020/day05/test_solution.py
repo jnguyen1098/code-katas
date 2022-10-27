d8ac3cf95311936a82b4a136c1bec7945980ec9e
Jason Nguyen <jnguyen1098@gmail.com>
2022-10-27 16:14:57 -0400

    bootstrap test data for p1

--- advent_of_code_2020/day05/test_solution.py
+++ advent_of_code_2020/day05/test_solution.py
@@ -6,8 +6,11 @@ from solution import *
 class TestAll(unittest.TestCase):
 
     def test_solve(self):
-        self.assertEqual(solve(1, "input"), 1)
-        self.assertEqual(solve(2, "input"), 2)
+        self.assertEqual(solve(1, "example1"), 357)
+        self.assertEqual(solve(1, "example2"), 567)
+        self.assertEqual(solve(1, "example3"), 119)
+        self.assertEqual(solve(1, "example4"), 820)
+        self.assertEqual(solve(1, "example4"), -1)
 
 if __name__ == "__main__":
     unittest.main(failfast=True)
