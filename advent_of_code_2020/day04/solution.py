aa6e9037d008ddb5ca520297c6b94139ac734680
Jason Nguyen <jnguyen1098@gmail.com>
2022-10-27 15:14:52 -0400

    tokenization

--- advent_of_code_2020/day04/solution.py
+++ advent_of_code_2020/day04/solution.py
@@ -6,6 +6,21 @@ sys.path.append("..")
 from ansi import *
 from comp import *
 
+def split_lines_into_key_value_pairs(lines):
+    kv = []
+
+    for line in lines:
+        if line.strip() == "":
+            continue
+        for token in (tokens := line.split(" ")):
+            key, value = token.split(":")
+            kv.append((key, value))
+
+    return kv
+
+def get_passports(raw_lines):
+    return []
+
 def solve(prob, inputname):
     lines = []
     gen = yield_line(inputname)
@@ -13,7 +28,9 @@ def solve(prob, inputname):
     for line in gen:
         lines.append(line)
 
-    print_arr(lines)
+    passports = get_passports(lines)
+
+    print_arr(passports)
 
     print(f"{len(lines)} in the array")
 
--- advent_of_code_2020/day04/test_solution.py
+++ advent_of_code_2020/day04/test_solution.py
@@ -3,7 +3,35 @@
 import unittest
 from solution import *
 
+def generate_example_lines():
+    lines = [
+        "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+        "byr:1937 iyr:2017 cid:147 hgt:183cm",
+        "",
+        "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+        "hcl:#cfa07d byr:1929",
+        "",
+        "hcl:#ae17e1 iyr:2013",
+        "eyr:2024",
+        "ecl:brn pid:760753108 byr:1931",
+        "hgt:179cm",
+        "",
+        "hcl:#cfa07d eyr:2025 pid:166559648",
+        "iyr:2011 ecl:brn hgt:59in",
+    ]
+    return lines
+
 class TestAll(unittest.TestCase):
+
+    def test_split_lines_into_key_value_pairs(self):
+        lines = generate_example_lines()
+        self.assertEqual(
+            split_lines_into_key_value_pairs([lines[0], "", lines[1]]),
+            [
+                ("ecl", "gry"), ("pid", "860033327"), ("eyr", "2020"), ("hcl", "#fffffd"),
+                ("byr", "1937"), ("iyr", "2017"), ("cid", "147"), ("hgt", "183cm"),
+            ]
+        )
 
     def test_solve(self):
         self.assertEqual(solve(1, "input"), 1)
