1bd97dda33eb039700623b794e7cc2c347a78c5d
Jason Nguyen <jnguyen1098@gmail.com>
2022-10-27 23:14:21 -0400

    part 1

--- advent_of_code_2020/day08/solution.py
+++ advent_of_code_2020/day08/solution.py
@@ -7,21 +7,38 @@ from ansi import *
 from comp import *
 
 def solve(prob, inputname):
-    lines = []
+    code = []
     gen = yield_line(inputname)
 
     for line in gen:
         if line == "":
             continue
         inst, val = parse(r"(nop|acc|jmp) ([\-\+]\d+)", line)
-        print(inst, val)
-
-    print_arr(lines)
-
-    print(f"{len(lines)} in the array")
+        code.append((inst, int(val)))
 
     if prob == 1:
-        return 1
+
+        def get_next_state(state):
+            pc, cum = state
+
+            inst, val = code[pc]
+            if inst == "nop":
+                return (pc + 1, cum)
+            if inst == "acc":
+                return (pc + 1, cum + val)
+            if inst == "jmp":
+                return (pc + val, cum)
+            raise Exception(f"Invalid state {state} yielded {inst=} {val=}")
+
+        state = (0, 0)  # program counter, accumulator
+
+        states = set([0])
+        while True:
+            if (new_state := get_next_state(state))[0] in states:
+                return state[1]
+            states.add(new_state[0])
+            state = new_state
+        
     elif prob == 2:
         return 2
     else:
--- advent_of_code_2020/day08/test_solution.py
+++ advent_of_code_2020/day08/test_solution.py
@@ -7,7 +7,7 @@ class TestAll(unittest.TestCase):
 
     def test_solve(self):
         self.assertEqual(solve(1, "example"), 5)
-        self.assertEqual(solve(1, "input"), 1)
+        self.assertEqual(solve(1, "input"), 1337)
         self.assertEqual(solve(2, "example"), 2)
         self.assertEqual(solve(2, "input"), 2)
 
